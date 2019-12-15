4854dce49203a2e50860a41d5d66051b6783915f
bpugh <bp@brandonpugh.com>
2019-12-16 01:51:21 +0900

    Only show latest 4 blog posts on home page
    I had to create a separate component since there currently isn't anyway
    in gatsby to reuse the static query by passing an argument

---
+++ src/components/HomePageBlogRoll.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { graphql, StaticQuery } from 'gatsby'
+import { BlogRoll } from './BlogRoll'
+
+/**
+ * Currently this component only differs from the base `BlogRoll` in that it only
+ * retrieves 4 posts instead of all of them in graphql query.
+ * Unfortunately there currently isn't anyway in gatsby to reuse the static query by passing an
+ * argument
+ */
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query HomePageBlogRollQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+          limit:4
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 400)
+              id
+              fields {
+                slug
+              }
+              timeToRead
+              frontmatter {
+                title
+                templateKey
+                date(formatString: "MMMM DD, YYYY")
+                featuredpost
+                featuredimage
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => <BlogRoll data={data} count={count} />}
+  />
+)
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
-import BlogRoll from '../components/BlogRoll'
+import HomePageBlogRoll from '../components/HomePageBlogRoll'
 
 export const IndexPageTemplate = ({
   image,
@@ -72,7 +72,7 @@ export const IndexPageTemplate = ({
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest stories
                   </h3>
-                  <BlogRoll />
+                  <HomePageBlogRoll />
                   <div className="column is-12 has-text-centered">
                     <Link className="btn" to="/blog">
                       Read more
