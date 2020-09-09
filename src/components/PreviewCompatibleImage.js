8b4396e38fbea4e1fb47c83ec982104b0d8d05ed
Brandon Pugh <bp@brandonpugh.com>
2020-09-09 17:03:32 +0200

    Lazy load preview images
    Make use of the relatively new native image lazy loading for images that
    don't use the scale up loading effect.
    Left it off the hero images since it's only recommended for images below
    the fold

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -17,7 +17,7 @@ const PreviewCompatibleImage = ({ imageInfo }) => {
   }
 
   if (!!image && typeof image === 'string')
-    return <img style={imageStyle} src={image} alt={alt} />
+    return <img loading="lazy" style={imageStyle} src={image} alt={alt} />
 
   return null
 }
